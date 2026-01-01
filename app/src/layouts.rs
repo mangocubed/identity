cbcff48f74759ec5c2c58fc595ac21fd9d01cb3a
Javier E. <jaedns@gmail.com>
2026-01-01 14:26:29 -0300

    Update layouts.rs

--- app/src/layouts.rs
+++ app/src/layouts.rs
@@ -36,7 +36,7 @@ pub fn LoginLayout() -> Element {
 
             div { class: "flex grow flex-col overflow-auto",
                 main { class: "main grow", Outlet::<Routes> {} }
-    
+
                 Footer {
                     aside { class: "opacity-75",
                         p {
@@ -46,20 +46,20 @@ pub fn LoginLayout() -> Element {
                             {env!("GIT_REV_SHORT")}
                             ")"
                         }
-    
+
                         p {
                             "Built on: "
                             {env!("BUILD_DATETIME")}
                         }
-    
+
                         p { {COPYRIGHT} }
                     }
-    
+
                     nav {
                         a { class: "link", href: TERMS_URL, target: "_blank", "Terms of Service" }
-    
+
                         a { class: "link", href: PRIVACY_URL, target: "_blank", "Privacy Policy" }
-    
+
                         a {
                             class: "link",
                             href: SOURCE_CODE_URL.clone(),
@@ -97,7 +97,7 @@ pub fn UserLayout() -> Element {
                             Brand { "ID" }
                         }
                     }
-    
+
                     NavbarEnd {
                         Dropdown { class: "dropdown-end",
                             div { class: "flex gap-2 items-center", tabindex: 0,
@@ -108,10 +108,10 @@ pub fn UserLayout() -> Element {
                                         {user.username.clone()}
                                     }
                                 }
-    
+
                                 ChevronDownMini {}
                             }
-    
+
                             DropdownContent { class: "mt-4",
                                 ul { class: "menu p-2", tabindex: 0,
                                     li {
@@ -125,13 +125,13 @@ pub fn UserLayout() -> Element {
                                 }
                             }
                         }
-    
+
                         ConfirmationModal {
                             is_open: show_logout_confirmation,
                             on_accept: move |()| {
                                 async move {
                                     let _ = run_with_spinner("logout", server_fns::logout).await;
-    
+
                                     delete_session();
                                     current_user.restart();
                                 }
@@ -140,7 +140,7 @@ pub fn UserLayout() -> Element {
                         }
                     }
                 }
-    
+
                 div { class: "flex grow overflow-auto",
                     div { class: "shrink-0 bg-base-200 rounded-box md:w-56 flex flex-col items-between",
                         ul { class: "menu md:w-56",
@@ -149,34 +149,34 @@ pub fn UserLayout() -> Element {
                                 "data-tip": "Home",
                                 Link { to: Routes::home(),
                                     HomeOutline {}
-    
+
                                     span { class: "max-md:hidden", "Home" }
                                 }
                             }
-    
+
                             div { class: "divider m-1" }
-    
+
                             li {
                                 class: "max-md:tooltip max-md:tooltip-right",
                                 "data-tip": "Email",
                                 Link { to: Routes::email(),
                                     EnvelopeOutline {}
-    
+
                                     span { class: "max-md:hidden", "Email" }
                                 }
                             }
-    
+
                             li {
                                 class: "max-md:tooltip max-md:tooltip-right",
                                 "data-tip": "Change password",
                                 Link { to: Routes::change_password(),
                                     PasswordOutline {}
-    
+
                                     span { class: "max-md:hidden", "Change password" }
                                 }
                             }
                         }
-    
+
                         ul { class: "menu md:w-56 mt-auto",
                             li {
                                 class: "max-md:tooltip max-md:tooltip-right",
@@ -186,17 +186,17 @@ pub fn UserLayout() -> Element {
                                         *show_about.write() = true;
                                     },
                                     InformationCircleOutline {}
-    
+
                                     span { class: "max-md:hidden", "About" }
                                 }
                             }
                         }
                     }
-    
+
                     main { class: "main grow max-w-[calc(100%-48px)] md:max-w-[calc(100%-208px)] overflow-y-scroll overflow-x-hidden",
                         Outlet::<Routes> {}
                     }
-    
+
                     AboutModal { is_open: show_about }
                 }
             }
